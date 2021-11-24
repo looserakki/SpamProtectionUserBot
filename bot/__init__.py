8e3d2a50510e21a2c62ea6898f745f93f9d2393a
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-11-24 18:10:18 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+import time
+from pyrogram import Client, errors
+from config import Config
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+StartTime = time.time()
+
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+SESSION = Config.SESSION
+
+app = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
