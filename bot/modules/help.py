589d7de25f00acbd4b581e05bde3445fa360a430
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-11-24 18:41:23 +0530

    forget ig

---
+++ bot/modules/help.py
@@ -0,0 +1,14 @@
+import asyncio
+from pyrogram import filters
+from Bot import app
+
+
+@app.on_message(filters.me & filters.regex("^\.help$"))
+def mainhelp(_, m):
+    m.edit(infohelptext)
+
+infohelptext = f"""
+『 INFO STUFF 』
+  .spb -> get info of a person from Intellivoid SpamProtection API.
+  .ping -> to see bot working or not.
+"""
