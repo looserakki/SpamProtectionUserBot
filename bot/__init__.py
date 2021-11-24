eb03b01f4e3c6d86e75c19e7e15642876afe3508
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-11-24 18:39:02 +0530

    hmm

--- bot/__init__.py
+++ bot/__init__.py
@@ -2,19 +2,22 @@ import logging
 import sys
 import time
 from pyrogram import Client, errors
-import config
+from config import API_HASH, API_ID, SESSION
+import logging
+
 import logging
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    filename="app.txt",
+    level=logging.ERROR,
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
 )
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
+LOGGER = logging.getLogger(__name__)
 
 StartTime = time.time()
 
-API_ID = config.API_ID
-API_HASH = config.API_HASH
-SESSION = config.SESSION
+API_ID = API_ID
+API_HASH = API_HASH
+SESSION = SESSION
 
 app = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
